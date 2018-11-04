57072054c39226bf8835e6a6027294b23c4f3bad
DavidEK123 <44358088+DavidEK123@users.noreply.github.com>
2018-11-03 20:44:20 -0700

    Update enders_keyboard.py

--- enders_keyboard.py
+++ enders_keyboard.py
@@ -9,29 +9,29 @@ def search(n):
     if n==1:
         browser.get('http://www.google.com')
         search = browser.find_element_by_name("q")
-        search.send_keys("hell world")
+        search.send_keys("hello world")
         search.submit()
 
     elif n==2:
         browser.get('http://www.youtube.com')
         search = browser.find_element_by_name("search_query")
-        search.send_keys("hell world")
+        search.send_keys("hello world")
         search.submit()
 
     elif n==3:
         browser.get('https://github.com')
         search = browser.find_element_by_name("q")
-        search.send_keys("hell world")
+        search.send_keys("hello world")
         search.submit()
 
     elif n==4:
         browser.get('https://www.wikipedia.org/')
         search = browser.find_element_by_name("search")
-        search.send_keys("hell world")
+        search.send_keys("hello world")
         search.submit() 
 
     elif n==5:
         browser.get('https://www.twitch.tv/')
         search = browser.find_element_by_id("nav-search-input")
-        search.send_keys("hell world")
+        search.send_keys("hello world")
         search.submit() 
